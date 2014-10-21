687bddd50be84856af863960dcdab1a07259d9c5
asand017 <asand017@ucr.edu>
2014-10-21 16:28:27 -0700

    rshell.cpp done more correctly now

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -9,39 +9,64 @@
 #include <sys/wait.h>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace boost;
 using namespace std;
 
-int main(int argc, char** argv)
+int main()
 {
-	if(argc == 0) {
-		cout << "No argument." << endl;
-		exit(1);
-	}
-
-	stringstream input;
-	for(int i = 0; i < argc; ++i)
+	string args;
+	while(1 != 2)
 	{
-		input << argv[i] << " ";
-	}
+		cout << "$ ";
+		getline(cin, args); 	
+	//stringstream input;
+	//for(int i = 0; i < argc; ++i)
+	//{
+	//	input << argv[i] << " ";
+	//}
 	
-	string sent; 
+//	string sent; 
 
-	input >> sent;
+//	input >> sent;
+		char *argv[9];
+		int i = 0;
+		vector<string> arg_s;	
+		tokenizer<> tok(args);
+		for(tokenizer<>::iterator beg=tok.begin(); beg!=tok.end();++beg) {
+			arg_s.push_back(*beg);
+			argv[i] = new char[6];
+			strcpy(argv[i], const_cast<char*>(arg_s[i].c_str()));
+		//	cout << arg_s[i] << endl;
+			++i;
+		}
+		argv[i] = NULL;	
+		int pid = fork();
+		if(pid == -1) {
+			perror("fork");
+			exit(1);
+		}
+		if(pid == 0) {
+			int r = execvp(argv[0], argv);
+			if(r == -1) {
+				perror("execvp");
+				exit(1);
+			}
+		}
+		else {
+			if(-1 == waitpid(pid, &pid, 0)) {
+				perror("waitpid");
+			}
+		}
 
-	tokenizer<> tok(sent);
-	for(tokenizer<>::iterator beg=tok.begin(); beg!=tok.end();++beg) {
-		cout  << *beg <<  "\n";
-	}
-		
-	int pid = fork();
-	if(pid == -1) {
-		perror("fork");
-		exit(1);
-	}
-	if(pid == 0)
-	{
+//	int pid = fork();
+//	if(pid == -1) {
+//		perror("fork");
+//		exit(1);
+//	}
+//	if(pid == 0)
+//	{
 		//char *argv2[4];
 		//argv2[0] = new char[6];
 		//strcpy(argv[0], "ls");
@@ -50,33 +75,33 @@ int main(int argc, char** argv)
 		//argv2[2] = new char [6];
 		//strcpy(argv[2], "-l");
 	
-		int pid2 = fork();
-		if(pid2 == -1) {
-			perror("fork");
-			exit(1);
-		}	
-		if(pid2 == 0) {
-			if(execvp(argv[0], argv) == -1) {
-				perror("execvp");	
-				exit(1);
-			}
-		}
-		else {
-			if(-1 == waitpid(pid2,&pid2,0 ))
-				perror("waitpid");
-		}
-		
-	        if(execvp(argv[1], argv) == -1) {
-			perror("execvp");
-			exit(1);
-		}
-
-		cout << "after" << endl;
+	//	int pid2 = fork();
+	//	if(pid2 == -1) {
+	//		perror("fork");
+	//		exit(1);
+	//	}	
+	//	if(pid2 == 0) {
+	///		if(execvp(argv[0], argv) == -1) {
+	//			perror("execvp");	
+	//			exit(1);
+	//		}
+	//	}
+	//	else {
+	//		if(-1 == waitpid(pid2,&pid2,0 ))
+	//			perror("waitpid");
+	//	}
+	//	
+	  //      if(execvp(argv[1], argv) == -1) {
+	//		perror("execvp");
+	//		exit(1);
+	//	}
+//
+//		cout << "after" << endl;
+//	}
+//	else {
+//		if(-1 == waitpid(pid, &pid, 0))
+//			perror("waitpid");
+//	}
 	}
-	else {
-		if(-1 == waitpid(pid, &pid, 0))
-			perror("waitpid");
-	}
-
 	return 0;
 }
