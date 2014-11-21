7ad8feff139c9db81dc10facd4b1053c0e9f0ea7
asand017 <asand017@ucr.edu>
2014-11-20 22:32:11 -0800

    yield

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -113,52 +113,62 @@ void rshell(string &x) {
 			//	going += *tok_iter;
 				if(*tok_iter == "<") {
 					//EXTRA CREDIT 1 HERE!!!!!!!!!!
-					char *buf = new char[sizeof(BUFSIZ)];
-					int fd2 = open(".manny", O_CREAT|O_RDWR|O_TRUNC, S_IRUSR|S_IWUSR);
-					if(fd2 == -1) {
-						perror("open");
-						exit(1);
-					}
+				//	char *buf = new char[sizeof(BUFSIZ)];
+				//	int fd5 = open("ok.string", O_CREAT | O_WRONLY, S_IWUSR);
+				//	if(fd5 == -1) {
+				//		perror("open");
+				//		exit(1);
+				//	}
 					
-					int bag = write(fd2, buf, BUFSIZ); 
-					if(bag == -1) {
-						perror("write");
-						exit(1);
-					}
+				//	int bag = write(fd5, buf, BUFSIZ); 
+				//	if(bag == -1) {
+				//		perror("write");
+				//		exit(1);
+				//	}
 					++tok_iter;
 					going += *tok_iter;
 					string leggo;
-					if(*tok_iter == "\"") {
+				if(*tok_iter == "\"") {
+				//	if(dup2(fd5, 0) == -1)
+				//		perror("dup2");
+					
+				//	int savestdin = dup(0);
+				//	if(savestdin == -1)
+				//		perror("dup");
+				//	if(*tok_iter == "\"") {	
 						++tok_iter;
 						while(*tok_iter != "\"") {
-							int bag = write(fd2, buf, BUFSIZ); 
-							if(bag == -1) {
-								perror("write");
-								exit(1);
-							}	
+						//	int bag = write(fd5, buf, BUFSIZ); 
+						//	if(bag == -1) {
+						//		perror("write");
+						//		exit(1);
+						//	}	
 	
 							cout << *tok_iter << endl;
 							leggo += *tok_iter;
 							leggo += " ";
 							++tok_iter;
 						}
-						//leggo += *tok_iter;
-						//going += " ";
-						//going += leggo;
+						leggo += *tok_iter;
+						going += " ";
+						going += leggo;
+						cout << leggo << endl;
 						//int bag = write(fd2, buf, BUFSIZ);
 						//if(bag == -1) {
 						//	perror("write");
 						//	exit(1);
 						//}
 					}
-					int claw = read(fd2, buf, BUFSIZ);
-					if(claw == -1)
-						perror("read");
+				//	int claw = read(fd2, buf, BUFSIZ);
+				//	if(claw == -1)
+				//		perror("read");
 					
-					int mk = close(fd2);
-					if(mk == -1)
-						perror("close");
-					delete [] buf;
+			//		int mk = close(fd5);
+			//		if(mk == -1)
+			//			perror("close");
+			//		delete [] buf;
+			//		if(dup2(savestdin, 0) == -1)
+			//			perror("dup2");
 				}
 			}
 			string cake = *tok_iter;
@@ -185,6 +195,113 @@ void rshell(string &x) {
 			continue;
 		  }
 	
+
+		  if(*tok_iter == "0") {
+			if(*tok_iter == ">") {
+				++tok_iter;
+				if(*tok_iter == ">") {
+					++tok_iter;
+					string swerve = *tok_iter;
+					int fd2 = open(swerve.c_str(), O_RDWR|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+					if(fd2 == -1) {
+						perror("open");
+						exit(1);
+					}
+					int grag = dup2(fd2, 0);
+					if(grag == -1) {
+						perror("dup2");
+						exit(1);
+					}
+					continue;
+				}
+				else {	
+					string hold = *tok_iter;
+					int fd = open(hold.c_str(), O_RDWR|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+					if(fd == -1) {
+						perror("open");
+						exit(1);
+					}
+					int drag = dup2(fd, 0);
+					if(drag == -1) {
+						perror("dup2");
+						exit(1);
+					}
+					continue;
+				}
+		  	}
+
+	
+		  }
+
+		  if(*tok_iter == "1") {
+			if(*tok_iter == ">") {
+				++tok_iter;
+				if(*tok_iter == ">") {
+					++tok_iter;
+					string swerve = *tok_iter;
+					int fd2 = open(swerve.c_str(), O_RDWR|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+					if(fd2 == -1) {
+						perror("open");
+						exit(1);
+					}
+					int grag = dup2(fd2, 1);
+					if(grag == -1) {
+						perror("dup2");
+						exit(1);
+					}
+					continue;
+				}
+				else {	
+					string hold = *tok_iter;
+					int fd = open(hold.c_str(), O_RDWR|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+					if(fd == -1) {
+						perror("open");
+						exit(1);
+					}
+					int drag = dup2(fd, 1);
+					if(drag == -1) {
+						perror("dup2");
+						exit(1);
+					}
+					continue;
+				}
+		  	}	
+		  }	
+		
+		  if(*tok_iter == "2") {
+			if(*tok_iter == ">") {
+				++tok_iter;
+				if(*tok_iter == ">") {
+					++tok_iter;
+					string swerve = *tok_iter;
+					int fd2 = open(swerve.c_str(), O_RDWR|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+					if(fd2 == -1) {
+						perror("open");
+						exit(1);
+					}
+					int grag = dup2(fd2, 2);
+					if(grag == -1) {
+						perror("dup2");
+						exit(1);
+					}
+					continue;
+				}
+				else {	
+					string hold = *tok_iter;
+					int fd = open(hold.c_str(), O_RDWR|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+					if(fd == -1) {
+						perror("open");
+						exit(1);
+					}
+					int drag = dup2(fd, 2);
+					if(drag == -1) {
+						perror("dup2");
+						exit(1);
+					}
+					continue;
+				}
+		  	}	
+		  }
 
 		  if(*tok_iter == ">") {
 			++tok_iter;
@@ -224,161 +341,151 @@ void rshell(string &x) {
 			tokenizer::iterator coward;
 			if(*tok_iter != "|") {
 			//piping
-			int pipefd[2];
-			pid_t cpid;
-			//char buf;
-			if(pipe(pipefd) == -1) {
-				perror("pipe");
-				exit(1);
-			}
-			cpid = fork();
-			if(cpid == -1) {
-				perror("fork");
-				exit(1);
-			}
-			else if(cpid == 0) {
-				if(-1 == dup2(pipefd[1],1))
-				{
-					perror("dup2");
-					exit(1);
-				}
-				if(-1 == close(pipefd[0]))
-				{
-					perror("close");
-					exit(1);
-				}
-				argv[i] = NULL;
-				if(-1 == execvp(argv[0], argv))
-				{
-					perror("execvp");
-				}
-				exit(1);
-			}
-			else if(cpid > 0)
-			{
-				int savestdin;
-				if(-1 == (savestdin = dup(0)))
-				{
-					perror("dup");
-					exit(1);
-				}
-				if(-1 == dup2(pipefd[0], 0)) {
-					perror("dup2");
-					exit(1);
-				}
-				if(-1 == close(pipefd[1])) {
-					perror("close");
-					exit(1);
-				}
-				if(-1 == waitpid(cpid, &cpid, 0)) {
-					perror("waitpid");
-					exit(1);
-				}
-				int pipefd2[2];
-				pid_t cpid2;
-				if(pipe(pipefd2) == -1) {
+				int pipefd[2];
+				pid_t cpid;
+				//char buf;
+				if(pipe(pipefd) == -1) {
 					perror("pipe");
 					exit(1);
 				}
-				cpid2 = fork();
-				if(cpid2 == -1) {
+				cpid = fork();
+				if(cpid == -1) {
 					perror("fork");
 					exit(1);
 				}
-				else if(cpid2 == 0) {
-					if(-1 == dup2(pipefd2[1], 1))
+				else if(cpid == 0) {
+					if(-1 == dup2(pipefd[1],1))
 					{
 						perror("dup2");
 						exit(1);
 					}
-					if(-1 == close(pipefd2[0])) 
+					if(-1 == close(pipefd[0]))
 					{
 						perror("close");
 						exit(1);
 					}
-					coward = tok_iter;
-					char *toby[9];
-					int l = 0;
-					vector<string> baby;
-					string mok;
-					while(tok_iter != tokens.end()) {
-						if(*tok_iter == "|")
-							break;
-						//cerr << *tok_iter << endl;
-						if((*tok_iter).size() == 1) {
-							mok += *tok_iter;
-						//	cerr << mok << endl;
-							++tok_iter;
-							mok += *tok_iter;
-						//	cerr << mok << endl;
-							++tok_iter;
-							mok += *tok_iter;
-							mok += " ";
-						//	cerr << mok << endl;
-							++tok_iter;
-							baby.push_back(mok);
-							toby[l] = new char[12];
-							strcpy(toby[l], const_cast<char*>(baby[l].c_str()));
-							++l;
-							cerr << toby[l] << "   " << "TOBY!!!!!!" << endl;
-							mok.erase(mok.begin(), mok.end());
-							continue;
-						}
-						mok += *tok_iter; //NEED TO PARSE MOK TO EXECUTE EXECVP - COME BACK LATER
-						cerr << mok << endl;
-						//++tok_iter;
-						baby.push_back(mok);
-						toby[l] = new char [12];
-						strcpy(toby[l], const_cast<char*>(baby[l].c_str()));
-						//going += mok;
-						cerr << toby[l] << "   " << "EFWQFEWFWFDFSAFSA" << endl;
-						++l;
-						mok.erase(mok.begin(), mok.end());
-						++tok_iter;
-					}
-					cerr << mok << "HEERRRRRREEEEEE" << endl;
-					//rshell(mok);
-					cerr << "BLAH BLAH BLAH" << endl;
-					//cerr << toby[0] << endl;
-					toby[l] = NULL;
-					if(-1 == execvp(toby[0], toby))	
+					argv[i] = NULL;
+					if(-1 == execvp(argv[0], argv))
+					{
 						perror("execvp");
-					
-					cerr << "DID IT!" << endl;
-						
+					}
 					exit(1);
 				}
-				else if(cpid > 0) {
+				else if(cpid > 0)
+				{
 					int savestdin;
 					if(-1 == (savestdin = dup(0)))
 					{
 						perror("dup");
 						exit(1);
 					}
-					if(-1 == dup2(pipefd2[0], 0)) {
+					if(-1 == dup2(pipefd[0], 0)) {
 						perror("dup2");
 						exit(1);
 					}
-					if(-1 == close(pipefd2[1])) {
+					if(-1 == close(pipefd[1])) {
 						perror("close");
 						exit(1);
 					}
-					if(-1 == waitpid(cpid2, &cpid2, 0)) {
+					if(-1 == waitpid(cpid, &cpid, 0)) {
 						perror("waitpid");
 						exit(1);
 					}
+				
+					int pipefd2[2];
+					pid_t cpid2;
+					string temp;
+					if(pipe(pipefd2) == -1) {
+						perror("pipe");
+						exit(1);
+					}
+					cpid2 = fork();
+					if(cpid2 == -1) {
+						perror("fork");
+						exit(1);
+					}
+					else if(cpid2 == 0) {
+						if(-1 == dup2(pipefd2[1], 1))
+						{
+							perror("dup2");
+							exit(1);
+						}
+						if(-1 == close(pipefd2[0])) 
+						{
+							perror("close");
+							exit(1);
+						}
+						while(tok_iter != tokens.end()) {
+							//temp += *tok_iter;
+							cerr << temp << endl;
+							if((*tok_iter).size() == 1) {
+								temp += *tok_iter; //A	
+								++tok_iter;
+								temp += *tok_iter; //A-
+								++tok_iter;
+								temp += *tok_iter; //A-Z
+								temp += " ";
+								++tok_iter;
+							}
+							else
+							{
+								temp += *tok_iter;
+								temp += " ";
+								++tok_iter;
+							}
+						}
+							cout << temp << endl;
+							char *toby[9];
+							int l = 0;
+							string goin;
+						//	string saad;
+							vector<string> ar;	
+						//	typedef tokenizer< char_separator<char> > tokenizer;
+							char_separator<char> sep (" ", "<>>>\"#-;||&&", drop_empty_tokens);
+							tokenizer tokens(temp, sep);
+							for(tokenizer::iterator tok_iter2=tokens.begin(); tok_iter2 != tokens.end(); ++tok_iter2) {
+								 ar.push_back(*tok_iter2);
+								 toby[l] = new char[12];
+	 	 						 strcpy(toby[l], const_cast<char*>(ar[l].c_str()));
+		  						 ++l;
+							}
+							toby[l] = NULL;
+							if(-1 == execvp(toby[0], toby)) {
+								perror("execvp");
+							}
+						
+							exit(1);
+					}
+					else if(cpid > 0) {
+						int savestdin;
+						if(-1 == (savestdin = dup(0)))
+						{
+							perror("dup");
+							exit(1);
+						}
+						if(-1 == dup2(pipefd2[0], 0)) {
+							perror("dup2");
+							exit(1);
+						}
+						if(-1 == close(pipefd2[1])) {
+							perror("close");
+							exit(1);
+						}
+						if(-1 == waitpid(cpid2, &cpid2, 0)) {
+							perror("waitpid");
+							exit(1);
+						}
 					//if(-1 == dup2(savestdout, 1))
 					//	perror("dup2");
-				}
-					//if(-1 == dup2(savestdin, 0))
+					}
+							//if(-1 == dup2(savestdin, 0))
 					//	perror("dup2");
-				}
+				
 				//tok_iter = coward;
-				continue;
-			}
-			//end piping	
-
-			if(*tok_iter == "|") {
+					continue;
+				}
+			}//end piping	
+			else if(*tok_iter == "|") {
 				++tok_iter;
 				rshell(going);
 				going.erase(0, going.find(*tok_iter, i));
@@ -422,7 +529,7 @@ void rshell(string &x) {
 
 		  going += *tok_iter;
 		  going += " ";
-		  cout << going << endl;	
+//		  cout << going << endl;	
 		  
 		  arg_s.push_back(*tok_iter);
 		  argv[i] = new char[12];
